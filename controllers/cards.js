c2f48c2851d5341abc2b217b06268f19630dd914
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-10-28 15:27:17 +0700

    add put and delete likes for card

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,3 +18,15 @@ module.exports.deleteCard = (req, res) => {
     .then(cards => res.send({ data: cards }))
     .catch(err => res.status(500).send({ message: err.message }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Cards.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then(cards => res.send({ data: cards }))
+    .catch(err => res.status(500).send({ message: err.message }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Cards.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then(cards => res.send({ data: cards }))
+    .catch(err => res.status(500).send({ message: err.message }));
+};
